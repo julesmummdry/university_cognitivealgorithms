53ffdd9ca280b72d284557f294369d18a5f2d2a8
jules <juliane@julesmacair.com>
2016-10-13 18:53:49 +0200

    changes in data file

--- ada16/ada16.py
+++ ada16/ada16.py
@@ -1,18 +1,20 @@
 #ada16
+#https://github.com/zyfang/AdaLovelace2016
 
 import pylab as pl
 import numpy as np
 import scipy as sp
-
-a = 2
-b = "two"
-
-c = str(a) + b
 
 def ada(watermelon_size):
     remaining = watermelon_size * 0.8
     print remaining
     return remaining
 
-ada(5)
+
+ada_data = np.genfromtxt('ada_data_short.csv', dtype=None, names=True, delimiter=',')
+
+print ada_data.size
+
+
+
 
