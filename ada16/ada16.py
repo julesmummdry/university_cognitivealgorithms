d55ebe31a4e38b22ceec870dc89f738ca5572534
jules <juliane@julesmacair.com>
2016-10-13 19:10:24 +0200

    added scatter plot

--- ada16/ada16.py
+++ ada16/ada16.py
@@ -5,6 +5,7 @@ import pylab as pl
 import numpy as np
 import scipy as sp
 
+
 def ada(watermelon_size):
     remaining = watermelon_size * 0.8
     print remaining
@@ -12,6 +13,9 @@ def ada(watermelon_size):
 
 
 ada_data = np.genfromtxt('ada_data_short.csv', dtype=None, names=True, delimiter=',')
+
+pl.scatter(ada_data["NCD_0"],ada_data["AI_0"])
+pl.show()
 
 print ada_data.size
 
