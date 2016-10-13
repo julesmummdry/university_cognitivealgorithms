04c8a2f296409564bd6bba744a587a146f209fd3
jules <juliane@julesmacair.com>
2016-10-13 18:31:11 +0200

    git ignor, added ada16 data and first imports

--- ada16/ada16.py
+++ ada16/ada16.py
@@ -1,1 +1,18 @@
 #ada16
+
+import pylab as pl
+import numpy as np
+import scipy as sp
+
+a = 2
+b = "two"
+
+c = str(a) + b
+
+def ada(watermelon_size):
+    remaining = watermelon_size * 0.8
+    print remaining
+    return remaining
+
+ada(5)
+
